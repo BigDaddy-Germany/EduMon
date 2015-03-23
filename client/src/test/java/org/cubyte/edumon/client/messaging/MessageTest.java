5b36dc24e7b2495e0cac81b352c7b9288d87dfbe
Phillip Schichtel <phillip.public@schich.tel>
2015-03-23 20:01:54 +0100

    Fixed a deprecation in the unit test

--- client/src/test/java/org/cubyte/edumon/client/messaging/MessageTest.java
+++ client/src/test/java/org/cubyte/edumon/client/messaging/MessageTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.StringWriter;
 
-import static junit.framework.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class MessageTest {
     @Test
