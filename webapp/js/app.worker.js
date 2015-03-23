5000b9c81b3d6c1422aa26a0bb0c86cef5729ea6
n2code <n2code@users.noreply.github.com>
2015-03-23 17:31:03 +0100

    english comments and app worker cleanup

--- webapp/js/app.worker.js
+++ webapp/js/app.worker.js
@@ -1,20 +1,39 @@
+/* Message-Worker communicates with the message server */
+
+//config
+var url = "demoDataIn.json";
+var outgoing = [];
+var interval = 1000;
+var requests_failed = 0;
 
 
-function createRequest(url) {
+/* Create a JSON-POST-HTTP-Request */
+function createRequest() {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', url, true);
     xhr.withCredentials = true;
+    xhr.contentType = 'application/json';
+    xhr.accepts = 'application/json';
+    xhr.dataType = 'json';
     return xhr;
 }
 
 
-
-var outgoing = [];
-
+/* Handle call for action from main app */
 onmessage = function(e) {
-    outgoing.push(e.data);
+	//Configuration command
+	if ("command" in e){
+		if (e.command=="config"){
+			url = e.url;
+		}
+	//Packet to queue
+	} else {
+		outgoing.push(e.data);
+	}
 };
 
+
+/* Process incoming paket[-array] */
 function handleEvent(event) {
     if (event instanceof Array) {
         for (var i = 0; i < event.length; ++i) {
@@ -25,28 +44,29 @@ function handleEvent(event) {
     }
 }
 
+
+/* Continuous sending of queued packets */
 setInterval(function() {
     var toBeSent = outgoing;
     outgoing = [];
 
-    var req = createRequest('demoDataIn.json');
+    var req = createRequest();
 
     function onError() {
-        console.log("Failed to send events to server, requeuing...");
+		requests_failed++;
         outgoing = outgoing.concat(toBeSent);
     }
 
-    req.onload = function() {
+    function onLoad() {
         if (req.status == 200) {
             handleEvent(JSON.parse(req.responseText))
         } else {
             onError();
         }
     };
+
     req.onerror = onError;
-    req.contentType = 'application/json';
-    req.accepts = 'application/json';
-    req.dataType = 'json';
+	req.onload = onLoad;
     req.send(JSON.stringify(toBeSent));
 
-}, 1000);
+}, interval);
