d14087a25fad3a05b5401fc51ee550fe198ff921
n2code <n2code@users.noreply.github.com>
2015-03-25 21:28:10 +0100

    closures make me CRAZY - seriously

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -1,114 +1,126 @@
 (function() {
 
-	/* Constructor */
+	/* Packet handling - Messager class connects with message worker */
+	function Messenger(eventCallback){
+		var that = this;
+
+		////////////////////private vars
+		var _worker;
+
+		////////////////////public vars
+
+		////////////////////private methods
+
+		////////////////////public methods
+
+		/* Send command to message worker */
+		this.sendEvent = function(event){
+			_worker.postMessage(event);
+		};
+
+
+		/* Destroy message worker */
+		this.kill = function(){
+			_worker.terminate();
+		}
+
+
+		////////////////////Constructor
+		_worker = new Worker('js/app.messageworker.js');
+		_worker.callbackWrapper = eventCallback;
+		_worker.onmessage = function(e) {
+			this.callbackWrapper(e.data);
+		};
+
+	}
+
+
+
+	/* Main app class */
 	function EduMon() {
-		var self = this;
+		var that = this;
+
+		////////////////////private vars
+		var session_id = ""; //session id assinged by message server //TODO: necessary?
+
+		////////////////////public vars
 		this.show_debug = true; //show debug messages in javascript console
-		this.session_id = ""; //session id assinged by message server //TODO: necessary?
 
-		/* Packet handling - Messager class connects with message worker */
-		this.Messenger = (function(){
-			//private vars
-			var _cback, _worker;
+		////////////////////private methods
 
-			//private methods
-			function _eventCallback(event){
-				self.handleIncomingData(event);
-			};
+		////////////////////public methods
 
-			//public vars & methods
-			return {
-				/* Constructor */
-				"init": function(){
-					_cback = _eventCallback;
-					_worker = new Worker('js/app.messageworker.js');
-					_worker.onmessage = function(e) {
-						_eventCallback(e.data);
-					};
-				},
-					/* Send command to message worker */
-					"sendEvent": function(event){
-						_worker.postMessage(event);
-					},
-					/* Destroy message worker */
-					"kill": function(){
-						_worker.terminate();
-					}
-			};
-		}());
-
-		this.debug("EduMon awakening...");
-		this.Messenger.init();
-	};
-
-
-	/* Debug output to JS console */
-	EduMon.prototype.debug = function(msg){
-		if (this.show_debug){
-			console.log(msg);
-		}
-	};
-
-
-	/* EduMon startup */
-	EduMon.prototype.init = function(){
-		var self = this;
-
-		this.debug("*** All Glory to the EduMon! ***");
-	};
-
-
-	/* Handle incoming data */
-	EduMon.prototype.handleIncomingData = function(event){
-		//Process packets received by message server
-		if ("inbox" in event && event.inbox.length > 0){
-			this.debug("Received packet[s]:");
-			this.debug(event.inbox);
-			for (var i = 0; i < event.inbox.length; ++i) {
-				this.processPacket(event.inbox[i]);
+		/* Debug output to JS console */
+		this.debug = function(msg){
+			if (that.show_debug){
+				console.log(msg);
 			}
-		}
+		};
 
-		//Handle any errors
-		if ("errorMessages" in event && event.errorMessages.length > 0){
-			for (var i = 0; i < event.errorMessages.length; ++i) {
-				this.debug(event.errorMessages[i]);
+
+		/* EduMon startup */
+		this.init = function(){
+			that.debug("*** All Glory to the EduMon! ***");
+			that.debug("EduMon awakening...");
+			that.Messenger = new Messenger(this.handleIncomingData);
+		};
+
+
+		/* Handle incoming data */
+		this.handleIncomingData = function(event){
+			//Process packets received by message server
+			if ("inbox" in event && event.inbox.length > 0){
+				for (var i = 0; i < event.inbox.length; ++i) {
+					that.processPacket(event.inbox[i]);
+				}
 			}
-		}
 
-		//Save session id assigned by message server
-		if ("clientId" in event){
-			this.session_id = event.clientId;
-		}
+			//Handle any errors
+			if ("errorMessages" in event && event.errorMessages.length > 0){
+				for (var i = 0; i < event.errorMessages.length; ++i) {
+					that.debug(event.errorMessages[i]);
+				}
+			}
+
+			//Save session id assigned by message server
+			if ("clientId" in event){
+				that.session_id = event.clientId;
+			}
+		};
+
+
+		/* Process packet */
+		this.processPacket = function(packet){
+			//TODO to be implemented
+			that.debug("Received packet:");
+			that.debug(packet);
+		};
+
+
+		/* [DEV] Send demo packet */
+		this.sendDemo = function(typenumber){
+			jQuery.getJSON("js/demoDataOut.json",function(data){
+				that.sendPacket(data["demo_type_"+typenumber]);
+			});
+		};
+
+
+		/* Queue packet for sending */
+		this.sendPacket = function(packet){
+			that.cmdConnection(packet);
+		};
+
+
+		/* Command message worker */
+		this.cmdConnection = function(cmd){
+			that.Messenger.sendEvent(cmd);
+		};
+
+
+		////////////////////Constructor
+		//moved to init to ensure dom-ready start
 	}
 
-
-	/* Process packet */
-	EduMon.prototype.processPacket = function(event){
-		//TODO to be implemented
-	}
-
-
-	/* [DEV] Send demo packet */
-	EduMon.prototype.sendDemo = function(typenumber){
-		var self = this;
-
-		jQuery.getJSON("js/demoDataOut.json",function(data){
-			self.sendPacket(data["demo_type_"+typenumber]);
-		});
-	}
-
-
-	/* Queue packet for sending */
-	EduMon.prototype.sendPacket = function(packet){
-		this.cmdConnection(packet);
-	}
-
-
-	/* Command message worker */
-	EduMon.prototype.cmdConnection = function(cmd){
-		this.Messenger.sendEvent(cmd);
-	}
 
 
 	window.EduMon = new EduMon();
