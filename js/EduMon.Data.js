74ff6dbd5a58068b5dbe2666d7c92c12a3a947c4
Cube Island Jenkins <development@cubeisland.de>
2015-03-30 15:24:43 +0200

    Synced webapp with gh-pages

--- js/EduMon.Data.js
+++ js/EduMon.Data.js
@@ -1,21 +1,39 @@
 window.EduMon.Data = new function Data() {
 	/**
-	 * Class to represent a student
+	 * Creates a new student object
 	 * @param {String} studentName the student's name
 	 * @param {String} group the group, the student is in
 	 */
 	this.Student = function Student(studentName, group) {
-		this.studentName = studentName;
-		this.group = group;
+		return {
+			studentName: studentName,
+			group: group
+		}
 	};
 
 	/**
-	 * Class to represent a course
+	 * Creates a new course object
 	 * @param {String} courseName A name to identify the course
-	 * @param {Student[]} students An array of all students inside the course
+	 * @param {int[]} students An array of all students (idx) in the course
 	 */
 	this.Course = function Course(courseName, students) {
-		this.courseName = courseName;
-		this.students = students;
+		return {
+			courseName: courseName,
+			students: students
+		};
+	};
+
+	/**
+	 * Creates a new room object
+	 * @param {String} roomName The room's name
+	 * @param {int} width The room's seats per row
+	 * @param {int} height The room's rows
+	 */
+	this.Room = function Room(roomName, width, height) {
+		return {
+			roomName: roomName,
+			width: width,
+			height: height
+		};
 	};
 };
