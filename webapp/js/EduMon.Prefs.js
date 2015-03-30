9d7f4cf9e5994caec79930c421033eff505c7501
Marco Dörfler <marco@doerflers.net>
2015-03-30 15:24:06 +0200

    First data draft, not finished yet

---
+++ webapp/js/EduMon.Prefs.js
@@ -0,0 +1,8 @@
+window.EduMon.Prefs = {
+    courses: [],        // an array of all courses
+    rooms: [],          // an array of all rooms
+    lectures: [],       // an array of all lectures
+
+    currentLecture: {}  // the current running lecture (copy everything!)
+
+};
