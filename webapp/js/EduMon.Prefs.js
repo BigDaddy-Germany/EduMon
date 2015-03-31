2273129d2c516f4797474e7e0e2db39dc3a222ae
Marco Dörfler <marco@doerflers.net>
2015-03-31 09:24:13 +0200

    CurrentLecture Copy is now creatable
    In order to work on #62

--- webapp/js/EduMon.Prefs.js
+++ webapp/js/EduMon.Prefs.js
@@ -2,6 +2,7 @@ window.EduMon.Prefs = {
     courses: [],        // an array of all courses
     rooms: [],          // an array of all rooms
     lectures: [],       // an array of all lectures
+    students: [],       // an array of all students
 
     currentLecture: {}  // the current running lecture (copy everything!)
 
