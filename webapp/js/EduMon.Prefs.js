6d9cd7f0088fa6b1ffa96bf7a83e67a2103041a5
Marco Dörfler <marco@doerflers.net>
2015-04-12 01:25:50 +0200

    Got rid of students[]

--- webapp/js/EduMon.Prefs.js
+++ webapp/js/EduMon.Prefs.js
@@ -2,7 +2,6 @@ EduMon.Prefs = {
     courses: [],        // an array of all courses
     rooms: [],          // an array of all rooms
     lectures: [],       // an array of all lectures
-    students: [],       // an array of all students
 
     currentLecture: {}  // the current running lecture (copy everything!)
 
