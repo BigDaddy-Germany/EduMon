558d07025085d2561d09deb36c3e1969ed7ca841
Cube Island Jenkins <development@cubeisland.de>
2015-04-11 19:48:52 +0200

    Synced webapp with gh-pages

--- js/EduMon.js
+++ js/EduMon.js
@@ -1,6 +1,15 @@
 EduMon = new function() {
 	var that = this;
 
+	var util;
+
+	var loginErrorCodes = {
+		nameAlreadyUsed: 1 << 0,
+		seatAlreadyUsed: 1 << 1,
+		nameNotAvailable: 1 << 2,
+		seatNotAvailable: 1 << 3
+	};
+
 	this.debugging = true; //show debug messages in javascript console
 
 	/* EduMon startup */
@@ -19,6 +28,7 @@ EduMon = new function() {
 		this.Timeline.init();
 
 		this.Analytics = new EduMon.Analytics();
+		util = EduMon.Util;
 	};
 
 
@@ -55,32 +65,32 @@ EduMon = new function() {
 		switch (packetType) {
 			case 2:
 				/*
-					client sends name and seat
-					body: { name: "..", seat: {x: 4, y: 1}}
+				 clients tries to log in
+				 body: { name: 'Some Name', seat: {x: 2, y: 3} }
 				 */
-				// todo do fancy things
+				processLogin(packet.from, packet.body);
 				break;
 
 			case 4:
 				/*
-					client sends data to server
-					body: { keys: 69, mdist: 999, mclicks: 23, volume: 0.42 }
+				client sends data to server
+				body: { keys: 69, mdist: 999, mclicks: 23, volume: 0.42 }
 				 */
 				EduMon.Analytics.processData(packet.from, packet.time, packet.body);
 				break;
 
 			case 6:
 				/*
-					Client sends feedback
-					body: { id: 123, value: 0.69 }
+				Client sends feedback
+				body: { id: 123, value: 0.69 }
 				 */
 				EduMon.Analytics.processFeedback(packet.from, packet.body);
 				break;
 
 			case 8:
 				/*
-					Client asks for break
-					body: {}
+				Client asks for break
+				body: {}
 				 */
 				// todo @Niko: Break Request
 				break;
@@ -88,6 +98,102 @@ EduMon = new function() {
 			default:
 				console.error('Unknown packet type ' + packetType + ' received.');
 		}
+	};
+
+
+	/**
+	 * Tries to login a client by its name and its seat. If
+	 * @param {String} sender the client's session id
+	 * @param {Object} body the body sent with the package to the server
+	 */
+	var processLogin = function(sender, body) {
+		var seat = body.seat;
+		var name = body.name;
+
+		var seatingPlan = EduMon.Prefs.currentLecture.seatingPlan;
+		var activeStudents = EduMon.Prefs.currentLecture.activeStudents;
+		var room = EduMon.Prefs.currentLecture.room;
+		var course = EduMon.Prefs.currentLecture.course;
+
+		var errorBits = new util.BitSet();
+
+        var responsePacket = {
+			type: 3,
+			time: Math.round(new Date().getTime() / 1000),
+			from: 'MODERATOR',
+			to: sender,
+			room: room.roomName,
+			body: {}
+		};
+
+		// check state of chosen seat
+		// if seat exists
+		if (seat.x <= room.width && seat.y <= room.height) {
+			seatingPlan[seat.x] = seatingPlan[seat.x] || [];
+
+			var seatState = seatingPlan[seat.x][seat.y];
+			// if seat is blocked
+			if (seatState != undefined) {
+				errorBits.set(loginErrorCodes.seatAlreadyUsed);
+			}
+		} else {
+			errorBits.set(loginErrorCodes.seatNotAvailable);
+		}
+
+
+		// check state of chosen name
+		var nameExists = false;
+		var groupForName;
+		course.students.forEach(function(studentId) {
+			var allStudents = EduMon.Prefs.students[studentId];
+            if (name == allStudents.studentName) {
+				nameExists = true;
+				groupForName = allStudents.group;
+			}
+		});
+
+		if (nameExists) {
+			var nameState;
+			util.forEachField(activeStudents, function (sessionId, student) {
+				if (student.studentName == name) {
+					nameState = sessionId;
+				}
+			});
+			if (nameState != undefined) {
+				errorBits.set(loginErrorCodes.nameAlreadyUsed);
+			}
+		} else {
+			errorBits.set(loginErrorCodes.nameNotAvailable);
+		}
+
+
+
+		// if seat and name are in use already, it's okay, if they are blocked by the client's session id
+		if (errorBits.contain(loginErrorCodes.nameAlreadyUsed | loginErrorCodes.seatAlreadyUsed)) {
+			if (nameState == seatState && seatState == sender) {
+				errorBits.clear(loginErrorCodes.nameAlreadyUsed | loginErrorCodes.seatAlreadyUsed)
+			}
+		}
+
+		// if error code is 0, user can be logged in
+		if (errorBits.equals(0)) {
+			activeStudents[sender] = activeStudents[sender] || {
+				studentName: name,
+				group: groupForName,
+				seat: seat,
+				disturbance: 0,
+				history: [],
+				micHistory: []
+			};
+
+			seatingPlan[seat.x] = seatingPlan[seat.x] || [];
+			seatingPlan[seat.x][seat.y] = seatingPlan[seat.x][seat.y] || sender;
+		}
+
+		responsePacket.body.successCode = errorBits.bits;
+
+		that.sendPacket(responsePacket);
+
 	};
 
 
