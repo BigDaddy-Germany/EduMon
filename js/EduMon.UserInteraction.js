d6b37e064668ef23c9c14ab3d14d14265b838220
Cube Island Jenkins <development@cubeisland.de>
2015-04-12 03:06:00 +0200

    Synced webapp with gh-pages

--- js/EduMon.UserInteraction.js
+++ js/EduMon.UserInteraction.js
@@ -52,6 +52,62 @@ EduMon.UserInteraction = new function() {
 
 
     /**
+     * Get updated data for new or existing course
+     * @param {int} [courseId] if update, the course
+     * @return {Promise} fulfill gets the updated course data
+     */
+    this.getCourseData = function(courseId) {
+
+        // get the values out of the fields
+        var valueCalculator = function() {
+            var courseName = $('#courseName').val();
+            var students = [];
+
+            $('.courseMemberLine').each(function() {
+                students.push({
+                    name: $(this).find('.courseMemberName').val(),
+                    group: $(this).find('.courseMemberGroup').val()
+                })
+            });
+
+            return EduMon.Data.Course(courseName, students);
+        };
+
+        // load saved course and initialize input fields
+        var initializer = function() {
+
+            // get the course data out of the preferences
+            var courseData = EduMon.Prefs.courses[courseId];
+            if (!courseData) {
+                return;
+            }
+
+            // set course name field
+            $('#courseName').val(courseData.name);
+
+            // add all fields, which are needed (one is there already)
+            var line = $('.courseMemberLine');
+            var lineHtml = line[0].outerHTML;
+            for (var i = 0; i < courseData.students.length; i++) {
+                if (i != 0) {
+                    line.after(lineHtml);
+                    line = line.next();
+                }
+                line.find('.courseMemberName').val(courseData.students[i].name);
+                line.find('.courseMemberGroup').val(courseData.students[i].group);
+            }
+
+        };
+
+        return new Promise(function(fulfill, reject) {
+            that.promisingDialog('editCourse', valueCalculator, initializer)
+                .then(fulfill)
+                .catch(reject);
+        });
+    };
+
+
+    /**
      * Opens a dialog and returns a promise to get the values entered by the user
      * @param {String} dialogId the dialog's ID
      * @param {Array} formIds all form IDs to return their content
@@ -93,9 +149,10 @@ EduMon.UserInteraction = new function() {
      * Opens a given dialog and executes the value calculator before closing it
      * @param {String} dialogId the ID to open the dialog
      * @param {Function} valueCalculator a function to calculate the values, the user entered
+     * @param {Function} [initializer] a function, which initializes the new opened dialog
      * @return {Promise} fulfill gets the calculated values
      */
-    this.promisingDialog = function(dialogId, valueCalculator) {
+    this.promisingDialog = function(dialogId, valueCalculator, initializer) {
         return new Promise(function(fulfill, reject) {
             var lastOpenedDialog = gui.getOpenedDialog();
             var promise;
@@ -107,6 +164,9 @@ EduMon.UserInteraction = new function() {
 
             promise
                 .then(function() {
+                    if (initializer) {
+                        initializer();
+                    }
                     $('#dialogBtnBack')
                         .off('click')
                         .on('click', function() {
