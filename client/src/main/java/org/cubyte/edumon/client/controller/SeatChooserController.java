7c03118b9c6347132fed1dc251a7b6192020a0fe
Jonas Dann <jonas.chr.dann@gmail.com>
2015-04-25 20:14:23 +0200

    Made the seatingplan graphicaly more pleasing

--- client/src/main/java/org/cubyte/edumon/client/controller/SeatChooserController.java
+++ client/src/main/java/org/cubyte/edumon/client/controller/SeatChooserController.java
@@ -9,6 +9,8 @@ import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
@@ -26,6 +28,9 @@ import static org.cubyte.edumon.client.Scene.LOGIN_CONFIRM;
 import static org.cubyte.edumon.client.Scene.NAME_CHOOSER;
 
 public class SeatChooserController implements Controller {
+    private static final String TABLE_IMAGE = "/table.png";
+    private static final Image tableImage;
+
     private Main app;
     @FXML
     private Pane pane;
@@ -33,6 +38,10 @@ public class SeatChooserController implements Controller {
     private Label roomAndName;
     @FXML
     private GridPane seatingplan;
+
+    static {
+        tableImage = new Image(Image.class.getResourceAsStream(TABLE_IMAGE));
+    }
 
     @FXML
     private void initialize() {
@@ -67,24 +76,39 @@ public class SeatChooserController implements Controller {
             public void run() {
                 final Dimensions dimensions = app.getRoomState().nameList.dimensions;
                 seatingplan.getChildren().removeAll(seatingplan.getChildren());
-                double rowHeight = 300d / dimensions.height;
                 double columnWidth = 573d / dimensions.width;
+                if (columnWidth > 130) {
+                    columnWidth = 130;
+                }
+                seatingplan.setPrefWidth(columnWidth * dimensions.width);
+                double rowHeight = 260d / dimensions.height;
+                if (rowHeight > 75) {
+                    rowHeight = 75;
+                }
+                seatingplan.setPrefHeight(rowHeight * dimensions.height);
                 Position seat = app.getSeat();
+
                 for (int x = 0; x < dimensions.width; x++) {
                     for (int y = 0; y < dimensions.height; y++) {
-                        final Hyperlink link = new Hyperlink(app.getName());
                         final int seatX = dimensions.width - x;
                         final int seatY = y + 1;
+
+                        final Label text = new Label(app.getName());
+                        text.setMinWidth(columnWidth);
+                        text.setMaxWidth(columnWidth);
+                        text.setMinHeight(rowHeight - (rowHeight / 4d));
+                        text.setStyle("-fx-alignment: top-center");
+                        text.setVisible(false);
+
+                        final Hyperlink link = new Hyperlink();
                         link.setPrefSize(columnWidth, rowHeight);
-                        link.setAlignment(Pos.CENTER);
-                        link.setStyle("-fx-text-fill: #fff;");
                         link.focusedProperty().addListener(new ChangeListener<Boolean>() {
                             @Override
                             public void changed(ObservableValue<? extends Boolean> observableValue, Boolean oldValue, Boolean newValue) {
                                 if (newValue) {
-                                    link.setStyle("-fx-text-fill: #000;");
+                                    text.setVisible(true);
                                 } else {
-                                    link.setStyle("-fx-text-fill: #fff;");
+                                    text.setVisible(false);
                                 }
                             }
                         });
@@ -102,7 +126,15 @@ public class SeatChooserController implements Controller {
                                 ((LoginConfirmController) LOGIN_CONFIRM.getController()).waitForConfirmation();
                             }
                         });
+
+                        final ImageView image = new ImageView(tableImage);
+                        image.setFitWidth(Math.ceil(columnWidth));
+                        image.setFitHeight(rowHeight);
+
+                        seatingplan.add(image, x, y);
+                        seatingplan.add(text, x, y);
                         seatingplan.add(link, x, y);
+
                         if (seat != null && seatX == seat.x && seatY == seat.y) {
                             link.requestFocus();
                         }
